574dcd79be23267f51ec228c837d2c01871addb7
divineadriano <adrianodivine@gmail.com>
2018-11-17 11:06:18 +0800

    initial

---
+++ README.md
@@ -0,0 +1,1 @@
+# proxibox-landing-page
---
+++ index.js
@@ -0,0 +1,63 @@
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+
+var app = express();
+
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+app.get('/', function(req, res) {
+	res.render('pages/home');
+});
+app.get('/home', (req, res) => res.render('pages/home'))
+
+app.get('/login', function(req, res) {
+	res.render('pages/login');
+});
+
+app.get('/register', function(req, res) {
+	res.render('pages/register');
+});
+
+app.get('/activate', function(req, res) {
+	res.render('pages/activate');
+});
+
+//kyc
+app.get('/kyc', function(req, res) {
+	res.render('pages/kyc');
+});
+
+//fileit
+app.get('/fileit', function(req, res) {
+	res.render('pages/fileit');
+});
+
+//suite
+app.get('/suite', function(req, res) {
+	res.render('pages/suite');
+});
+
+//notes
+app.get('/notes', function(req, res) {
+	res.render('pages/notes');
+});
+
+//forms
+app.get('/forms', function(req, res) {
+	res.render('pages/activate');
+});
+
+//keepass
+app.get('/vault', function(req, res) {
+	res.render('pages/vault');
+});
+
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  
+  
+
